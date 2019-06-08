136d69f049f27b1c93095723daffb75ea9fb7be6
ssahu9 <sumit.ps94@gmail.com>
2019-06-08 12:44:32 +0530

    Maven Project setup

---
+++ Junit5/src/main/java/learn/junit/MathUtils.java
@@ -0,0 +1,9 @@
+package learn.junit;
+
+public class MathUtils {
+
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
---
+++ Junit5/src/test/java/learn/junit/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package learn.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("TEST RUNNER updated");
+	}
+
+}
