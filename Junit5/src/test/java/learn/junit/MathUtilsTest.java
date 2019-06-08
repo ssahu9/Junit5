cfbb2dc0522af02b8cdd5fe31ba0c321546c7066
ssahu9 <sumit.ps94@gmail.com>
2019-06-08 15:10:35 +0530

    First Implementation of Assert Equal method

--- Junit5/src/test/java/learn/junit/MathUtilsTest.java
+++ Junit5/src/test/java/learn/junit/MathUtilsTest.java
@@ -8,6 +8,16 @@ class MathUtilsTest {
 
 	@Test
 	void test() {
+		// Create the instance of class
+		//Setup inputs 
+		// execute the code you want to test 
+		// comapre the result
+		
+		MathUtils obj = new MathUtils();
+		int expected =6;
+		int actual=obj.add(3, 3);
+		assertEquals(expected, actual);
+		System.out.println("TEST RUNNER updated");
 		System.out.println("TEST RUNNER updated");
 	}
 
