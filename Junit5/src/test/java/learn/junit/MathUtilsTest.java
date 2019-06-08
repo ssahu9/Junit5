1c862990df167e3484945e31f7c24f70b0cec168
ssahu9 <sumit.ps94@gmail.com>
2019-06-08 15:42:24 +0530

    Added maven surefire pluging - to run the test case during maven build and maven test

--- Junit5/src/test/java/learn/junit/MathUtilsTest.java
+++ Junit5/src/test/java/learn/junit/MathUtilsTest.java
@@ -17,8 +17,19 @@ class MathUtilsTest {
 		int expected =6;
 		int actual=obj.add(3, 3);
 		assertEquals(expected, actual);
+//		When it fails, it will generate This method should return sum message
+//		assertEquals(expected, actual, "This method should return sum  ");
+		
 		System.out.println("TEST RUNNER updated");
 		System.out.println("TEST RUNNER updated");
+	}
+	
+	@Test
+	void testComputeCircleArea() {
+		MathUtils obj = new MathUtils();
+		double expected = 153.93804002589985;
+		double actual = obj.computeCircleArea(7);
+		assertEquals(expected, actual,"This method should return area of circle");
 	}
 
 }
